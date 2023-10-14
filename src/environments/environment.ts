e59987e93fdc42aa662de7e09f0c420a1be16ab4
Andrei Domkin <Andrei_Domkin@epam.com>
2023-10-14 17:21:13 +0400

    fix: update url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -10,7 +10,7 @@ export const environment: Config = {
     product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    bff: 'https://1d6y1e8y9h.execute-api.eu-west-1.amazonaws.com/dev',
+    bff: 'https://mpgqy60s6i.execute-api.eu-west-1.amazonaws.com/dev',
     cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   },
   apiEndpointsEnabled: {
