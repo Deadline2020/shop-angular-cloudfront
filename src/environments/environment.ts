d3ceb73ad18e34b13536d9c060c37dea6df0d84a
Andrei Domkin <Andrei_Domkin@epam.com>
2023-10-22 14:02:48 +0400

    feat: change endpoint config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,14 +9,14 @@ export const environment: Config = {
   apiEndpoints: {
     product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+    import: 'https://mxht2xe2ue.execute-api.eu-west-1.amazonaws.com/dev',
     bff: 'https://mpgqy60s6i.execute-api.eu-west-1.amazonaws.com/dev',
     cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   },
   apiEndpointsEnabled: {
     product: false,
     order: false,
-    import: false,
+    import: true,
     bff: true,
     cart: false,
   },
